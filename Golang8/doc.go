a920c457abbb2c12d42e555c76b93f7038a4c23e
2119762713 <2119762713@qq.com>
2021-01-31 19:36:11 +0800

    Golang语言编写的留言本拥有增删查改四种功能.注意：数据库和表没有上传

---
+++ Golang8/doc.go
@@ -0,0 +1,6 @@
+// Golang8 project doc.go
+
+/*
+Golang8 document
+*/
+package main
---
+++ Golang8/utils/main.go
@@ -0,0 +1,18 @@
+// utils project main.go
+package main
+
+import (
+	"database/sql"
+	f "fmt"
+	"html/template"
+	"net/http"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	func Liebiao(w http.ResponseWriter, r *http.Request){
+		
+	}
+}
