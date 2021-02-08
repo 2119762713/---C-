cb33cbf99a5a19e21a2060e7b8b6e715422153eb
2119762713 <2119762713@qq.com>
2021-02-08 16:00:16 +0800

    C语言学习笔记代码6

---
+++ yun(6)/yun/add.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#pragma once
+
+
+
+//函数定义
+int add(int x, int y)//这样把函数放后面还会警告怎么办，我们在前面函数声明就行了。
+{
+	return  x + y;
+}
---
+++ yun(6)/yun/add.h
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#pragma once
+#ifndef __ADD_H_
+#define __ADD_H_
+
+
+//º¯ÊıÉùÃ÷
+int add(int x,int y);
+
+#endif
